3a7bfc7f09556de807a6bff0e151b8015e32a241
Jose Lara <jose@joselara.com>
2024-01-29 09:02:32 -0800

    Add Avatar component

---
+++ components/Avatar.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Avatar() {
+  return (
+    <div className="h-16 w-16 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
+      <Image src="/me.jpg" className="rounded-full bg-zinc-100 object-cover dark:bg-zinc-800" alt="Jose Lara Avatar" />
+    </div>
+  );
+};
