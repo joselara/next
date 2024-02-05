3dea0439dc1e08c9f10d7729a9c84bea869eedb9
Jose Lara <jose@joselara.com>
2024-02-04 20:26:34 -0800

    Update avatar component and remove next placeholder graphics

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
 export default function Avatar() {
   return (
-    <div className="h-16 w-16 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
-      <Image src="/me.jpg" className="rounded-full bg-zinc-100 object-cover dark:bg-zinc-800" alt="Jose Lara Avatar" />
+    <div className='h-16 w-16 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur'>
+      <Image
+        src='/me.jpg'
+        width='100'
+        height='100'
+        className='rounded-full bg-zinc-100 object-cover dark:bg-zinc-800'
+        alt='Jose Lara Avatar'
+      />
     </div>
-  );
-};
+  )
+}
