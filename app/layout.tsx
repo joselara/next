38a56662945820710f63d962b648d39b361e3485
Jose Lara <jose@joselara.com>
2024-02-12 08:51:54 -0800

    Remove unnecessary div from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={`font-sans antialiased ${inter.className}`}>
         <div className='fixed inset-0 flex justify-center sm:px-8'>
           <div className='flex w-full max-w-7xl lg:px-8'>
-            <div className='w-full bg-white ring-1 ring-zinc-100'></div>
+            <div className='w-full bg-white ring-1 ring-zinc-100' />
           </div>
         </div>
         <div className='relative'>
