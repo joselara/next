e9d78eaa40d75ac367cd4c8aa9cb257c6697d7fb
Jose Lara <jose@joselara.com>
2024-02-12 20:56:52 -0800

    Update prop names for WorkProfile component

--- components/WorkProfile.tsx
+++ components/WorkProfile.tsx
@@ -1,45 +1,45 @@
 import React from 'react'
 
 interface Props {
-  name: string
-  title: string
+  company: string
+  position: string
   location: string
   description?: string
-  start: string
-  end: string
-  bullets: string[]
+  startDate: string
+  endDate: string
+  responsibilities: string[]
 }
 
 const WorkProfile: React.FC<Props> = ({
-  name,
-  title,
+  company,
+  position,
   location,
   description,
-  start,
-  end,
-  bullets,
+  startDate,
+  endDate,
+  responsibilities,
 }) => {
   return (
     <div className='grid gap-3'>
       <div>
         <div className='text-2xl font-bold tracking-tight text-zinc-800 sm:text-3xl'>
-          {name}
+          {company}
         </div>
         <div className='text-xl font-semibold tracking-tight text-zinc-600 sm:text-2xl'>
-          {title}
+          {position}
         </div>
       </div>
       <div className='text-sm sm:text-base text-zinc-600'>
         <div>
-          {start} &mdash; {end}
+          {startDate} &mdash; {endDate}
         </div>
         <div>{location}</div>
       </div>
       <div>
         {description && <p>{description}</p>}
-        {bullets.length > 0 && (
+        {responsibilities.length > 0 && (
           <ul role='list' className='list-disc pl-4 text-zinc-600'>
-            {bullets.map((bullet, index) => (
+            {responsibilities.map((bullet, index) => (
               <li key={index}>{bullet}</li>
             ))}
           </ul>
